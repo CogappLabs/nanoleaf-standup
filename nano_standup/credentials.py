945103161d5e0631aa382a87b4ead7cc087427f1
benk <benk@cogapp.com>
2017-12-08 13:56:02 +0000

    Added RGB configuration to manifest format and standup script.

--- nano_standup/credentials.py
+++ nano_standup/credentials.py
@@ -13,6 +13,14 @@ def load_manifest(base_dir):
     try:
         with open(os.path.join(base_dir, 'manifest.yml'), 'r') as m:
             manifest = yaml.load(m)
+
+            for name, panel_data in manifest['panels'].items():
+                rgb = panel_data['rgb'].split(' ')
+                manifest['panels'][name]['rgb'] = {
+                    'R': rgb[0],
+                    'G': rgb[1],
+                    'B': rgb[2],
+                }
     except yaml.YAMLError as e:
         print('Error loading YAML: {!s}'.format(e))
     except Exception as e:
